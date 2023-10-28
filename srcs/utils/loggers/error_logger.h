2ca16f4a2810562bde806d7eb6950955d855d4e3
Tomoya Okuyama <nitunamoni13579@gmail.com>
2023-10-28 18:07:59 +0900

    tmp

--- srcs/utils/loggers/error_logger.h
+++ srcs/utils/loggers/error_logger.h
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 14:55:11 by tookuyam          #+#    #+#             */
-/*   Updated: 2023/10/28 15:18:31 by tookuyam         ###   ########.fr       */
+/*   Updated: 2023/10/28 18:07:51 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,13 @@ void	error_log_str(char *actual, char *expected);
 # define VALUE_OF_LOG(VALUE) \
 	fprintf(stderr, "Value of: %s\n", VALUE)
 
-
-
-
-#define BASIC_ERROR_LOG(VALUE, ACTUAL, EXPECTED) \
-	DETAIL_ERROR_LOG(__FILE__, __LINE__, VALUE, ACTUAL, EXPECTED)
+#define BASE_LOG(FILE, LINE, VALUE);
 
 #define BASIC_ERROR_LOG(FILE, LINE, VALUE, ACTUAL, EXPECTED) \
 do { \
 	PLACE_LOG(FILE, LINE, "Failure"); \
 	VALUE_OF_LOG(VALUE); \
-	ACTUAL_LOG(ACTUAL); \
-	EXPECTED_LOG(EXPECTED); \
+	error_log_bool(ACTUAL, EXPECTED); \
 } while(0)
 
 #endif
--- srcs/utils/loggers/error_loggers_num.c
+++ srcs/utils/loggers/error_loggers_num.c
@@ -1,24 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_loggers.c                                    :+:      :+:    :+:   */
+/*   error_loggers_num.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tookuyam <tookuyam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 15:12:04 by tookuyam          #+#    #+#             */
-/*   Updated: 2023/10/28 15:17:29 by tookuyam         ###   ########.fr       */
+/*   Updated: 2023/10/28 18:07:50 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	error_log_bool(int actual, int expected)
+void	error_log_num(char const *message, int value)
 {
-	fprintf(stderr, "Actual: %s\n", 
-		actual ? "true" : "false");
+	fprintf(stderr, "%s: %d\n", value);
+}
+
+void	error_log_bool(char const *message, int value)
+{
+	fprintf(stderr, "%s: %d\n", 
+		value ? "true" : "false");
+}
+
+void	error_log_expected(int expected)
+{
 	fprintf(stderr, "Expected: %s\n",
 		expected ? "true" : "false");
-	return ;
 }
 
 void	error_log_str(char *actual, char *expected)
