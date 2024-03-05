8d358148ddd31387d520ca1d46c685debb66122b
tookuyam <tookuyam@student.42tokyo.jp>
2024-03-06 04:28:37 +0900

    fix: EXCEPT

--- srcs/modules/expect_functions.h
+++ srcs/modules/expect_functions.h
@@ -19,128 +19,107 @@
 # include <string.h>
 # include "utils_str.h"
 
-/**
- * test that condition is equal to true.
- * @param condition condition
- */
-# define EXPECT_TRUE(condition) \
-	EXPECT_CHECK(condition, \
-		EXPECT_LOG("==", "true", condition, \
-			VALUE_LOG_STRING_RAW, \
-			VALUE_LOG_BOOL))
+# define __ERROR_MESSAGE "\033[33mError\033[m"
 
 /**
  * test that condition is equal to true.
  * @param condition condition
  */
-# define EXPECT_FALSE(condition) \
-	EXPECT_CHECK(!(condition), \
-		EXPECT_LOG("==", "false", condition, \
-			VALUE_LOG_STRING_RAW, \
-			VALUE_LOG_BOOL))
+# define EXPECT_TRUE(condition) do { \
+	TEST_BOOL(true, condition, "==", NUM_EQ, "assert_true", __ERROR_MESSAGE); \
+} while (0);
+
+
+/**
+ * test that condition is equal to true.
+ * @param condition condition
+ */
+# define EXPECT_FALSE(condition) do { \
+	TEST_BOOL(false, condition, "==", NUM_EQ, "assert_false", __ERROR_MESSAGE); \
+} while (0);
 
 /**
  * test that expected is equal to actual.
  * @param expected actual
  */
-# define EXPECT_EQ(expected, actual) \
-	EXPECT_CHECK((expected == actual), \
-		EXPECT_LOG("==", expected, actual, \
-			VALUE_LOG_NUM, \
-			VALUE_LOG_NUM))
+# define EXPECT_EQ(expected, actual) do { \
+	TEST_NUMBER(expected, actual, "==", NUM_EQ, "assert_eq", __ERROR_MESSAGE); \
+} while (0);
 
 /**
  * test that expected is not equal to actual.
  * @param expected actual
  */
-# define EXPECT_NE(expected, actual) \
-	EXPECT_CHECK((expected != actual), \
-		EXPECT_LOG("!=", expected, actual, \
-			VALUE_LOG_NUM, \
-			VALUE_LOG_NUM))
+# define EXPECT_NE(expected, actual) do {\
+	TEST_NUMBER(expected, actual, "!=", NUM_NE, "assert_ne", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is less than actual.
  * @param expected actual
  */
-# define EXPECT_LT(expected, actual) \
-	EXPECT_CHECK((expected < actual), \
-		EXPECT_LOG("<", expected, actual, \
-			VALUE_LOG_NUM, \
-			VALUE_LOG_NUM))
+# define EXPECT_LT(expected, actual) do {\
+	TEST_NUMBER(expected, actual, "<", NUM_LT, "assert_lt", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is less than or equal to actual.
  * @param expected actual
  */
-# define EXPECT_LE(expected, actual) \
-	EXPECT_CHECK((expected <= actual), \
-		EXPECT_LOG("<=", expected, actual, \
-			VALUE_LOG_NUM, \
-			VALUE_LOG_NUM))
+# define EXPECT_LE(expected, actual) do {\
+	TEST_NUMBER(expected, actual, "<=", NUM_LE, "assert_le", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is greater than to actual.
  * @param expected actual
  */
-# define EXPECT_GT(expected, actual) \
-	EXPECT_CHECK((expected > actual), \
-		EXPECT_LOG(">", expected, actual, \
-			VALUE_LOG_NUM, \
-			VALUE_LOG_NUM))
+# define EXPECT_GT(expected, actual) do {\
+	TEST_NUMBER(expected, actual, ">", NUM_GT, "assert_gt", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is greater than or equal to actual.
  * @param expected actual
  */
-# define EXPECT_GE(expected, actual) \
-	EXPECT_CHECK((expected >= actual), \
-		EXPECT_LOG(">=", expected, actual, \
-			VALUE_LOG_NUM, \
-			VALUE_LOG_NUM))
+# define EXPECT_GE(expected, actual) do {\
+	TEST_NUMBER(expected, actual, ">=", NUM_GE, "assert_ge", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is equal to actual.
  * @param expected expected value
  * @param actual actual value
  */
-# define EXPECT_STREQ(expected, actual) \
-	EXPECT_CHECK(strcmp(expected, actual) == 0, \
-		EXPECT_LOG("==", expected, actual, \
-			VALUE_LOG_STRING, \
-			VALUE_LOG_STRING))
+# define EXPECT_STREQ(expected, actual) do { \
+	TEST_STRING(expected, actual, "==", STR_EQ, "assert_streq", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is not equal to actual.
  * @param expected expected value
  * @param actual actual value
  */
-# define EXPECT_STRNE(expected, actual) \
-	EXPECT_CHECK(strcmp(expected, actual) != 0, \
-		EXPECT_LOG("!=", expected, actual, \
-			VALUE_LOG_STRING, \
-			VALUE_LOG_STRING))
+# define EXPECT_STRNE(expected, actual) do { \
+	TEST_STRING(expected, actual, "!=", STR_NE, "assert_strne", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is equal to actual. ignore character case.
  * @param expected expected value
  * @param actual actual value
  */
-# define EXPECT_STRCASEEQ(expected, actual) \
-	EXPECT_CHECK(stricmp(expected, actual) == 0, \
-		EXPECT_LOG("==", expected, actual, \
-			VALUE_LOG_STRING, \
-			VALUE_LOG_STRING))
+# define EXPECT_STRCASEEQ(expected, actual) do { \
+	TEST_STRING(expected, actual, "==", STR_CASEEQ, "assert_strcaseeq", __ERROR_MESSAGE); \
+} while (0)
 
 /**
  * test that expected is not equal to actual. ignore character case.
  * @param expected expected value
  * @param actual actual value
  */
-# define EXPECT_STRCASENE(expected, actual) \
-	EXPECT_CHECK(stricmp(expected, actual) != 0, \
-		EXPECT_LOG("!=", expected, actual, \
-			VALUE_LOG_STRING, \
-			VALUE_LOG_STRING))
+# define EXPECT_STRCASENE(expected, actual) do { \
+	TEST_STRING(expected, actual, "!=", STR_CASENE, "assert_strcaseeq", __ERROR_MESSAGE); \
+} while (0)
 
 #endif
